3796a5f02adc30928cec8220dad2b83fb22ca179
dewanakl <dewanakretarta29@gmail.com>
2025-06-17 08:59:12 +0700

    refactor: streamline video loading process and enhance caching logic

--- js/app/guest/video.js
+++ js/app/guest/video.js
@@ -9,48 +9,15 @@ export const video = (() => {
     let c = null;
 
     /**
-     * @type {HTMLVideoElement|null}
+     * @returns {void}
      */
-    let vid = null;
-
-    /**
-     * @returns {Promise<Response>}
-     */
-    const getVideo = () => {
-        const bar = document.getElementById('progress-bar-video-love-stroy');
-        const inf = document.getElementById('progress-info-video-love-stroy');
-
-        return request(HTTP_GET, vid.src).withRetry().withProgressFunc((a, b) => {
-            const result = Math.min((a / b) * 100).toFixed(0) + '%';
-
-            bar.style.width = result;
-            inf.innerText = result;
-        }).default().then((r) => c.set(vid.src, r));
-    };
-
-    /**
-     * @returns {Promise<void>}
-     */
-    const load = () => c.open().then(() => c.has(vid.src).then((res) => {
-        if (res) {
-            return res;
+    const load = () => {
+        const container = document.getElementById('video-love-stroy');
+        if (!container) {
+            return;
         }
 
-        return c.del(vid.src).then(() => getVideo());
-    }).then((r) => r.blob()).then((b) => {
-        vid.src = URL.createObjectURL(b);
-        vid.dispatchEvent(new Event('undangan.cache.video'));
-    }));
-
-    /**
-     * @returns {object}
-     */
-    const init = () => {
-        c = cache('video').withForceCache();
-
-        vid = document.createElement('video');
-        const container = document.getElementById('video-love-stroy');
-
+        const vid = document.createElement('video');
         vid.src = container.getAttribute('data-src');
         vid.className = container.getAttribute('data-vid-class');
         vid.controls = true;
@@ -62,21 +29,56 @@ export const video = (() => {
         vid.disableRemotePlayback = true;
         vid.controlsList = 'noremoteplayback nodownload noplaybackrate';
 
-        const observer = new IntersectionObserver((es) => es.forEach((e) => e.isIntersecting ? vid.play() : vid.pause()));
-
-        vid.addEventListener('undangan.cache.video', () => {
-            observer.observe(vid);
-            document.getElementById('video-love-stroy-loading')?.remove();
-        });
-
         vid.addEventListener('loadedmetadata', () => {
             const ratio = vid.videoHeight / vid.videoWidth;
             const width = vid.getBoundingClientRect().width;
 
             vid.style.height = `${width * ratio}px`;
+
+            container.removeAttribute('data-src');
+            container.removeAttribute('data-vid-class');
         });
 
         container.appendChild(vid);
+        const observer = new IntersectionObserver((es) => es.forEach((e) => e.isIntersecting ? vid.play() : vid.pause()));
+
+        /**
+         * @returns {Promise<Response>}
+         */
+        const fetchVideo = () => {
+            const bar = document.getElementById('progress-bar-video-love-stroy');
+            const inf = document.getElementById('progress-info-video-love-stroy');
+
+            return request(HTTP_GET, vid.src).withRetry().withProgressFunc((a, b) => {
+                const result = Math.min((a / b) * 100).toFixed(0) + '%';
+
+                bar.style.width = result;
+                inf.innerText = result;
+            }).default().then((r) => c.set(vid.src, r));
+        };
+
+        // run in async
+        c.open()
+            .then(() => c.has(vid.src).then((res) => {
+                if (res) {
+                    return res;
+                }
+
+                return c.del(vid.src).then(() => fetchVideo());
+            }))
+            .then((r) => r.blob())
+            .then((b) => {
+                vid.src = URL.createObjectURL(b);
+                observer.observe(vid);
+                document.getElementById('video-love-stroy-loading')?.remove();
+            });
+    };
+
+    /**
+     * @returns {object}
+     */
+    const init = () => {
+        c = cache('video').withForceCache();
 
         return {
             load,
