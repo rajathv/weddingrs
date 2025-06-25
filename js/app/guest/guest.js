c9e8ea822cc6664378c75486bacc50b21a30aa10
dewanakl <dewanakretarta29@gmail.com>
2025-06-25 11:45:19 +0700

    refactor: adjust font size in guest name display for improved readability

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -76,7 +76,7 @@ export const guest = (() => {
             const div = document.createElement('div');
             div.classList.add('m-2');
 
-            const template = `<small class="mt-0 mb-1 mx-0 p-0">${util.escapeHtml(guestName?.getAttribute('data-message'))}</small><p class="m-0 p-0" style="font-size: 1.5rem">${util.escapeHtml(name)}</p>`;
+            const template = `<small class="mt-0 mb-1 mx-0 p-0">${util.escapeHtml(guestName?.getAttribute('data-message'))}</small><p class="m-0 p-0" style="font-size: 1.25rem">${util.escapeHtml(name)}</p>`;
             util.safeInnerHTML(div, template);
 
             guestName?.appendChild(div);
